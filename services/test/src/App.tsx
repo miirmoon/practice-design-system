062301ac675447b16860778a2454ab92e273cdb6
shinmirae <mirae.shin@muzlive.com>
2024-01-16 09:40:22 +0900

    feat: color token 적용 테스트

---
+++ services/test/src/App.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+import { vars } from "@miirmoon/themes";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return <View />;
+}
+
+export default App;
+
+const View = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Text>font color is {vars.colors.$static.light.blue[500]}</Text>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const Text = styled.p`
+  color: ${vars.colors.$static.light.blue[500]};
+`;
