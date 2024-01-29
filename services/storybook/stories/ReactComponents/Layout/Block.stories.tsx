3ed7e78515d4ae169b34d99af9fbef5b8f051137
shinmirae <mirae.shin@muzlive.com>
2024-01-29 10:02:39 +0900

    Chore: storybook 설치

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import { Block as _Block } from "@miirmoon/react-components-layout"
+// import "@miirmoon/react-components-layout/styles.css"
+
+export default {
+  title: 'React Components/Layout/Block',
+  component: _Block,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ['autodocs']
+}
+
+export const BlockStory = {}
